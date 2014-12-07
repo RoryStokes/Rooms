605598d3e9e9c56952893d29bb28c0d119061a95
Rory Stokes <contact@rorystok.es>
2014-12-07 16:27:21 +1030

    server connection and assignment functional

--- server/server/urls.py
+++ server/server/urls.py
@@ -6,10 +6,10 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'server.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^game/', game),
+    url(r'^game', game),
     url(r'^init', init),
     url(r'^start', start),
     url(r'^state', state),
-    url(r'^connect/', connect),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^connect', connect),
+    url(r'^admin', include(admin.site.urls)),
 )
